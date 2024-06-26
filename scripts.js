b010ccf5ca75cb84f8b9dd396f68d663392f849d
Hector B <hectorbg303@gmail.com>
2024-06-26 21:13:27 +0200

    Select

--- scripts.js
+++ scripts.js
@@ -1,197 +1,262 @@
 const wordList = [
     {
         es: 'Saludo',
-        ko: '인사'
+        ko: '인사',
+        section: '1'
     },{
         es: 'Hola',
-        ko: '안녕하세요'
+        ko: '안녕하세요',
+        section: '1'
     },{
         es: 'Encantado',
-        ko: '반갑습니다'
+        ko: '반갑습니다',
+        section: '1'
     },{
         es: '¿Cómo te llamas?',
-        ko: '이름이 뭐에요?'
+        ko: '이름이 뭐에요?',
+        section: '1'
     },{
         es: 'Me llamo Héctor',
-        ko: '저는 헥토로 이에요'
+        ko: '저는 헥토로 이에요',
+        section: '1'
     },{
         es: 'Mi nombre es Héctor',
-        ko: '제 이름은 헥토로 이에요'
+        ko: '제 이름은 헥토로 이에요',
+        section: '1'
     },{
         es: 'Te veo luego',
-        ko: '다음에 또 만나요'
+        ko: '다음에 또 만나요',
+        section: '1'
     },
     {
         es: 'Adiós (me voy)',
-        ko: '안녕히 겨세요'
+        ko: '안녕히 겨세요',
+        section: '1'
     },
     {
         es: 'Adiós (se va)',
-        ko: '안녕히 가세요'
+        ko: '안녕히 가세요',
+        section: '1'
     },{
         es: 'Gracias',
-        ko: '감사합니다'
+        ko: '감사합니다',
+        section: '1'
     },{
         es: 'Lo siento',
-        ko: '죄송합니다'
+        ko: '죄송합니다',
+        section: '1'
     },{
         es: 'Ok',
-        ko: '괜잖 습니다'
+        ko: '괜잖 습니다',
+        section: '1'
     },{
         es: 'Si',
-        ko: '네'
+        ko: '네',
+        section: '1'
     },{
         es: 'No',
-        ko: '아니요'
+        ko: '아니요',
+        section: '1'
     },{
         es: '¿Cómo estás?',
-        ko: '어똥게 지내세요?'
+        ko: '어똥게 지내세요?',
+        section: '1'
     },{
         es: 'Estoy bien',
-        ko: '저는 잘 지내요'
+        ko: '저는 잘 지내요',
+        section: '1'
     },{
         es: 'Numeros',
-        ko: '숫자'
+        ko: '숫자',
+        section: '2'
     },{
         es: '1',
-        ko: '일'
+        ko: '일',
+        section: '2'
     },{
         es: '2',
-        ko: '이'
+        ko: '이',
+        section: '2'
     },{
         es: '3',
-        ko: '삼'
+        ko: '삼',
+        section: '2'
     },{
         es: '4',
-        ko: '사'
+        ko: '사',
+        section: '2'
     },{
         es: '5',
-        ko: '오'
+        ko: '오',
+        section: '2'
     },{
         es: '6',
-        ko: '육'
+        ko: '육',
+        section: '2'
     },{
         es: '7',
-        ko: '칠'
+        ko: '칠',
+        section: '2'
     },{
         es: '8',
-        ko: '팔'
+        ko: '팔',
+        section: '2'
     },{
         es: '9',
-        ko: '구'
+        ko: '구',
+        section: '2'
     },{
         es: '10',
-        ko: '십'
+        ko: '십',
+        section: '2'
     },{
         es: '11',
-        ko: '십 일'
+        ko: '십 일',
+        section: '2'
     },{
         es: '12',
-        ko: '십 이'
+        ko: '십 이',
+        section: '2'
     },{
         es: '20',
-        ko: '이 십'
+        ko: '이 십',
+        section: '2'
     },{
         es: '21',
-        ko: '이 십 일'
+        ko: '이 십 일',
+        section: '2'
     },{
         es: '30',
-        ko: '삼 십'
+        ko: '삼 십',
+        section: '2'
     },{
         es: '100',
-        ko: '백'
+        ko: '백',
+        section: '2'
     },{
         es: '200',
-        ko: '이 박'
+        ko: '이 박',
+        section: '2'
     },{
         es: '585',
-        ko: '오 백 팔 십 오'
+        ko: '오 백 팔 십 오',
+        section: '2'
     },{
         es: '1.000',
-        ko: '천'
+        ko: '천',
+        section: '2'
     },{
         es: '10.000',
-        ko: '만'
+        ko: '만',
+        section: '2'
     },{
         es: '100.000',
-        ko: '십 만'
+        ko: '십 만',
+        section: '2'
     },{
         es: '1.000.000',
-        ko: '백 만'
+        ko: '백 만',
+        section: '2'
     },{
         es: '10.000.000',
-        ko: '천 만'
+        ko: '천 만',
+        section: '2'
     },{
         es: '100.000.000',
-        ko: '억'
+        ko: '억',
+        section: '2'
     },{
-            es: '1 (contar)',
-        ko: '하 나'
+        es: '1 (contar)',
+        ko: '하 나',
+        section: '3'
     },{
         es: '2 (contar)',
-        ko: '둘'
+        ko: '둘',
+        section: '3'
     },{
         es: '3 (contar)',
-        ko: '셋'
+        ko: '셋',
+        section: '3'
     },{
         es: '4 (contar)',
-        ko: '넷'
+        ko: '넷',
+        section: '3'
     },{
         es: '5 (contar)',
-        ko: '다 섯'
+        ko: '다 섯',
+        section: '3'
     },{
         es: '6 (contar)',
-        ko: '여 삿'
+        ko: '여 삿',
+        section: '3'
     },{
         es: '7 (contar)',
-        ko: '일 곱'
+        ko: '일 곱',
+        section: '3'
     },{
         es: '8 (contar)',
-        ko: '여 덟'
+        ko: '여 덟',
+        section: '3'
     },{
         es: '9 (contar)',
-        ko: '아 홉'
+        ko: '아 홉',
+        section: '3'
     },{
         es: '10 (contar)',
-        ko: '열'
+        ko: '열',
+        section: '3'
     },{
         es: '17 (contar)',
-        ko: '열 일 곱'
+        ko: '열 일 곱',
+        section: '3'
     },{
         es: '20 (contar)',
-        ko: '스물'
+        ko: '스물',
+        section: '3'
     },{
         es: '30 (contar)',
-        ko: '서른'
+        ko: '서른',
+        section: '3'
     },{
         es: '40 (contar)',
-        ko: '마흔'
+        ko: '마흔',
+        section: '3'
     },{
         es: '50 (contar)',
-        ko: '쇤'
+        ko: '쇤',
+        section: '3'
     },{
         es: '60 (contar)',
-        ko: '여순'
+        ko: '여순',
+        section: '3'
     },{
         es: '70 (contar)',
-        ko: '일흔'
+        ko: '일흔',
+        section: '3'
     },{
         es: '80 (contar)',
-        ko: '여든'
+        ko: '여든',
+        section: '3'
     },{
         es: '90 (contar)',
-        ko: '아흔'
+        ko: '아흔',
+        section: '3'
     },{
         es: 'Colores',
-        ko: '색깔'
+        ko: '색깔',
+        section: '4'
     },{
         es: 'Rojo',
-        ko: '빨강'
+        ko: '빨강',
+        section: '4'
     },{
         es: 'Color rojo',
-        ko: '빨강 색'
+        ko: '빨강 색',
+        section: '4'
     }
 ];
+
+let shuffledList = [];
 
 let index = 0;
 let language = 'ko';
@@ -202,6 +267,7 @@ const nextElement = document.querySelector('.btn--next');
 const koElement = document.querySelector('.btn--ko');
 const esElement = document.querySelector('.btn--es');
 const soundElement = document.querySelector('.btn--sound');
+const selectElement = document.querySelector('.select');
 
 function start() {
     beforeElement.addEventListener('click', beforeWord);
@@ -209,29 +275,31 @@ function start() {
     koElement.addEventListener('click', languageKorean);
     esElement.addEventListener('click', languageSpanish);
     soundElement.addEventListener('click', converToSpeech);
+    selectElement.addEventListener('change', selectSection);
     
-    shuffle(wordList);
-    wordElement.innerHTML = wordList[index][language];
+    shuffledList = [...wordList];
+    shuffle(shuffledList);
+    wordElement.innerHTML = shuffledList[index][language];
 }
 
 function beforeWord() {
     index = (index === 0) ? 0 : index - 1;
-    wordElement.innerHTML = wordList[index][language];
+    wordElement.innerHTML = shuffledList[index][language];
 }
 
 function nextWord() {
-    index = (index === wordList.length - 1) ? wordList.length - 1 : index + 1;
-    wordElement.innerHTML = wordList[index][language];
+    index = (index === shuffledList.length - 1) ? shuffledList.length - 1 : index + 1;
+    wordElement.innerHTML = shuffledList[index][language];
 }
 
 function languageKorean() {
     language = 'ko';
-    wordElement.innerHTML = wordList[index][language];
+    wordElement.innerHTML = shuffledList[index][language];
 }
 
 function languageSpanish() {
     language = 'es';
-    wordElement.innerHTML = wordList[index][language];
+    wordElement.innerHTML = shuffledList[index][language];
 }
 
 function shuffle(array) {
@@ -248,8 +316,20 @@ function shuffle(array) {
 function converToSpeech() {
     const msg = new SpeechSynthesisUtterance();
     msg.lang = language === 'ko' ? 'ko-KR' : 'es-ES'
-    msg.text = wordList[index][language];
+    msg.text = shuffledList[index][language];
     window.speechSynthesis.speak(msg);
+}
+
+function selectSection(ev) {
+    const section = ev.target.value;
+    if(section) {
+        shuffledList = wordList.filter(word => word.section === section);
+    } else {
+        shuffledList = [...wordList];
+    }
+    shuffle(shuffledList);
+    index = 0;
+    wordElement.innerHTML = shuffledList[index][language];
 }
 
 start();
