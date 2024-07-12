713a2ba9c97dcf07cae3043af036d7e2f8f03821
Hector B <hectorbg303@gmail.com>
2024-07-13 00:42:40 +0200

    mejoras. Iconos y ojo

--- scripts.js
+++ scripts.js
@@ -551,6 +551,11 @@ function shuffle(array) {
 function converToSpeech() {
     const msg = new SpeechSynthesisUtterance();
     //msg.lang = language === 'ko' ? 'ko-KR' : 'es-ES';
+    if(speedElement.value > 10) {
+        speedElement.value = 10;
+    } else if(speedElement.value < 1) {
+        speedElement.value = 1;
+    }
     msg.lang = 'ko-KR';
     msg.rate = Number(speedElement.value) / 10;
     msg.text = shuffledList[index]['ko'];
@@ -583,7 +588,22 @@ function showModal() {
 function writeWord() {
     const row = `
             <tr>
-                <td><strong>${shuffledList[index]['ko']}</strong></td>
+                <td>
+                    <strong class="hidden">${shuffledList[index]['ko']}</strong>
+                    <span class="eye hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="icon-sm">
+                        <path d="M3.53 2.47a.75.75 0 0 0-1.06 1.06l18 18a.75.75 0 1 0 1.06-1.06l-18-18ZM22.676 12.553a11.249 11.249 0 0 1-2.631 4.31l-3.099-3.099a5.25 5.25 0 0 0-6.71-6.71L7.759 4.577a11.217 11.217 0 0 1 4.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113Z" />
+                        <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0 1 15.75 12ZM12.53 15.713l-4.243-4.244a3.75 3.75 0 0 0 4.244 4.243Z" />
+                        <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 0 0-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 0 1 6.75 12Z" />
+                        </svg>
+                    </span>
+                    <span class="eye">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="icon-sm">
+                        <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+                        <path fill-rule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z" clip-rule="evenodd" />
+                        </svg>
+                    </span>
+                </td>
                 <td>${shuffledList[index]['es']}</td>
                 <td class="delete">❌</td>
             </tr>
@@ -596,9 +616,15 @@ function writeWord() {
 
 function handleClickOnTable(ev) {
     const deleteButton = ev.target.closest('.delete');
-    if(!deleteButton) return;
-    deleteButton.closest('tr').remove();
-    if(!tableElement.innerHTML.trim()) hideModal();
+    const eyeButton = ev.target.closest('.eye');
+    if(deleteButton) {
+        deleteButton.closest('tr').remove();
+        if(!tableElement.innerHTML.trim()) hideModal();
+    } else if(eyeButton) {
+        for(let el of eyeButton.closest('td').children) {
+            el.classList.toggle('hidden')
+        }
+    }
 }
 
 function saveTableOnMemory() {
